6ad242bc5ce6710e2fed493f2f9ac223d91a9392
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-08 18:12:22 +0300

    update: toJson and toInstance functions

--- src/model/todo.model.js
+++ src/model/todo.model.js
@@ -10,8 +10,28 @@ export default class Todo {
     constructor(title) {
         this.title = title;
         this.status = "pending"
-        this.id = id++;
+        this.id = null;
         this.dateCreated = new Date();
         this.priority = TODO_IMPORTANCE_LOW;
+    }
+
+    toJson() {
+        return {
+            title: this.title,
+            status: this.status,
+            dateCreated: this.dateCreated,
+            priority: this.priority,
+        }
+    }
+
+    static toInstance({title, status, id, dateCreated, priority}) {
+        const todo = new Todo(title);
+
+        todo.status = status;
+        todo.id = id;
+        todo.dateCreated = dateCreated;
+        todo.priority = priority;
+
+        return todo;
     }
 }
