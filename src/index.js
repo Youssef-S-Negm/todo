8dcf04a977727ea6326a5fbe44b0f347bbeec49e
Youssef Negm <youssef.negm2001@gmail.com>
2025-05-08 19:40:40 +0300

    update: fetching todos on page load

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import { getAllTodos } from "./firebase/todo.service.js";
+import Todo from "./model/todo.model.js";
 import { setUpAddTodoForm, setupDropZone, setUpSearchForm, setUpSortActions } from "./setup/formsSetup.js";
+import state from "./state.js";
+import renderTodos from "./utils/render.js";
 
 setUpAddTodoForm();
 setUpSearchForm();
@@ -6,3 +10,19 @@ setUpSortActions();
 
 setupDropZone("pending");
 setupDropZone("completed");
+
+document.addEventListener("DOMContentLoaded", onStart)
+
+async function onStart() {
+    try {
+        const snapshot = await getAllTodos();
+
+        snapshot.forEach(doc => {
+            state.todos.push(Todo.toInstance({ ...doc.data(), id: doc.id }))
+        })
+
+        renderTodos();
+    } catch (error) {
+        console.error(error);
+    }
+}
